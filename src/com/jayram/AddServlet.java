ec7724bf7fffb7e48cc4c53b96a546e8eb767c00
JayramNanoskar <jayram.n978@gmail.com>
2020-08-30 20:47:51 +0530

    using sendRedirect with session | calling servlet from servlet

--- src/com/jayram/AddServlet.java
+++ src/com/jayram/AddServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.sun.media.sound.RealTimeSequencerProvider;
 
@@ -19,11 +20,11 @@ public class AddServlet extends HttpServlet
 	{
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
-		int k = i+j;
+		int k = i + j;
 		
-		res.sendRedirect("sq?k="+k);
+		HttpSession session = req.getSession();
+		session.setAttribute("k", k);
+		res.sendRedirect("sq");
 		
-//		RequestDispatcher rd = req.getRequestDispatcher("sq");
-//		rd.forward(req, res);
 	}
 }
